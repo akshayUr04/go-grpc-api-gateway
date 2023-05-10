14ad657a85b4e7d316174075bf7014ba57a92bb2
akshayUr04 <akshayur0404@gmail.com>
2023-05-10 13:57:38 +0530

    main

---
+++ cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+
+	"github.com/akshayUr04/go-grpc-api-gateway/pkg/auth"
+	"github.com/akshayUr04/go-grpc-api-gateway/pkg/config"
+	"github.com/akshayUr04/go-grpc-api-gateway/pkg/order"
+	"github.com/akshayUr04/go-grpc-api-gateway/pkg/product"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+
+	if err != nil {
+		log.Fatalln("Failed at config", err)
+	}
+	r := gin.Default()
+
+	authSvc := *auth.RegisterRoutes(r, &c)
+	product.RegisterRoutes(r, &c, &authSvc)
+	order.RegisterRoutes(r, &c, &authSvc)
+
+	r.Run(c.Port)
+}
